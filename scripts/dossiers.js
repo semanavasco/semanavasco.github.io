bc35cfb20eb2eeb20235238de2e3777ee0257a41
svasco <semanavasco@gmail.com>
2023-12-15 22:15:36 +0100

    Finished and optimized all commands showed in help

---
+++ scripts/dossiers.js
@@ -0,0 +1,54 @@
+dossiers = [
+  {
+    name: "~",
+    terminal: "~",
+    pwd: "/home/visitor",
+    type: "folder",
+    childs: ["AboutMe", "Studies", "Projects", "Ideas", "SocialMedia"],
+    ls: [
+      '<span class="comment">AboutMe</span>',
+      '<span class="comment">Studies</span>',
+      '<span class="comment">Projects</span>',
+      '<span class="comment">Ideas</span>',
+      '<span class="comment">SocialMedia</span>',
+    ],
+  },
+  {
+    name: "AboutMe",
+    terminal: "/home/visitor/AboutMe",
+    pwd: "/home/visitor/AboutMe",
+    type: "folder",
+    childs: ["whoami.txt"],
+    ls: ['<span class="normalized">whoami.txt</span>'],
+  },
+  {
+    name: "Studies",
+    terminal: "/home/visitor/Studies",
+    pwd: "/home/visitor/Studies",
+    type: "folder",
+    childs: ["HighSchool.txt", "University.txt"],
+    ls: [
+      '<span class="normalized">HighSchool.txt</span>',
+      '<span class="normalized">University.txt</span>',
+    ],
+  },
+  {
+    name: "Projects",
+    terminal: "/home/visitor/Projects",
+    pwd: "/home/visitor/Projects",
+    type: "folder",
+    childs: ["Personal.txt", "Professional.txt"],
+    ls: [
+      '<span class="normalized">Personal.txt</span>',
+      '<span class="normalized">Professional.txt</span>',
+    ],
+  },
+  {
+    name: "Ideas",
+    terminal: "/home/visitor/Ideas",
+    pwd: "/home/visitor/Ideas",
+    type: "folder",
+    childs: ["Games.txt"],
+    ls: ['<span class="normalized">Games.txt</span>'],
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,79 +1,5 @@
 var dossierActuel = "~";
 
-var dossiers = [
-  {
-    name: "~",
-    pwd: "/home/visitor",
-    type: "folder",
-    content: [
-      {
-        name: "AboutMe",
-        pwd: "/home/visitor/AboutMe",
-        type: "folder",
-        content: [
-          {
-            name: "AboutMe.txt",
-            type: "file",
-            content:
-              "Hello, my name is Semana Vasco. I am a 20 year old student from the Netherlands. I am currently studying Computer Science at the University of Amsterdam. I am a very curious person and I love to learn new things. I am also very passionate about programming and I am always looking for new challenges.",
-          },
-        ],
-      },
-      {
-        name: "Studies",
-        pwd: "/home/visitor/Studies",
-        type: "folder",
-        content: [
-          {
-            name: "Studies.txt",
-            type: "file",
-            content:
-              "I am currently studying Computer Science at the University of Amsterdam. I am in my second year of the Bachelor's programme and I am really enjoying it so far. I am also currently doing a minor in Artificial Intelligence.",
-          },
-        ],
-      },
-      {
-        name: "Projects",
-        pwd: "/home/visitor/Projects",
-        type: "folder",
-        content: [
-          {
-            name: "Projects.txt",
-            type: "file",
-            content: "I am currently working on a project called 'The Terminal'.",
-          },
-        ],
-      },
-      {
-        name: "Ideas",
-        pwd: "/home/visitor/Ideas",
-        type: "folder",
-        content: [
-          {
-            name: "Ideas.txt",
-            type: "file",
-            content:
-              "I have a lot of ideas for projects that I want to work on. I am currently working on a project called 'The Terminal'.",
-          },
-        ],
-      },
-      {
-        name: "SocialMedia",
-        pwd: "/home/visitor/SocialMedia",
-        type: "folder",
-        content: [
-          {
-            name: "SocialMedia.txt",
-            type: "file",
-            content:
-              "I am currently active on Instagram and LinkedIn. You can find my social media accounts in the footer of this website.",
-          },
-        ],
-      },
-    ],
-  }
-]
-
 document.addEventListener("DOMContentLoaded", function () {
   // Sélectionner l'élément input par son ID
   var zoneTexte = document.getElementById("zonetexte");
@@ -104,35 +30,68 @@ document.addEventListener("keydown", async function (event) {
       .appendChild(document.createElement("div"));
     ancienneLigne.classList.add("lignes");
     ancienneLigne.classList.add("ancienneLigne");
-    ancienneLigne.innerHTML = `<p><span class="information">visitor@semanavasco</span><span class="normalized">:</span><span class="comment">${dossierActuel}</span><span class="information">$</span> </p>
+    ancienneLigne.innerHTML = `<p><span class="information">visitor@semanavasco</span><span class="normalized">:</span><span class="comment">${
+      dossiers.find((dossier) => dossier.name === dossierActuel).terminal
+    }</span><span class="information">$</span> </p>
          <p>${commande}</p>`;
 
+    // Séparer la commande en arguments
+    var arguments = commande.split(" ");
+
     // Vérifier la commande
-    if (commande === "help") {
-      reponseTerminal(
-        `<p>
+    if (arguments[0] === "help") {
+      if (arguments.length > 1)
+        reponseTerminal(
+          `<p>   <span class="error">error: "help" command does not take any argument</span></p>`
+        );
+      else
+        reponseTerminal(
+          `<p>
            <span class="comment">help</span>           <span class="question">It seems like you already know what this does.</span><br/>
            <span class="comment">pwd</span>            <span class="question">Shows you the path of the current directory you are in within the file system.</span><br/>
            <span class="comment">ls</span>             <span class="question">Provides a listing of the contents of the specified directory.</span><br/>
            <span class="comment">cd</span>             <span class="question">Changes your current working directory to the specified directory.</span><br/>
            <span class="comment">clear</span>          <span class="question">Removes all previous commands and output from the terminal.</span><br/>
        </p>`
-      );
-    }
-    if (commande === "ls") {
-      if (dossierActuel === "~")
-        reponseTerminal(
-          `<p>
-           <span class="question">AboutMe</span>   <span class="question">Studies</span>   <span class="question">Projects</span>   <span class="question">Ideas</span>   <span class="question">SocialMedia</span>
-       </p>`
         );
-    } else if (commande === "cd") {
-      reponseTerminal(
-        `<p>
-           <span class="error">error: "cd" command needs a parameter</span>
-       </p>`
-      );
-    } else if (commande === "clear") {
+    } else if (arguments[0] === "ls") {
+      if (arguments.length === 1)
+        reponseTerminal(
+          `<p>   ${dossiers
+            .find((dossier) => dossier.name === dossierActuel)
+            .ls.join("   ")}</p>`
+        );
+      else if (
+        dossierActuel === "~" &&
+        dossiers
+          .find((dossier) => dossier.name === dossierActuel)
+          .childs.includes(arguments[1])
+      ) {
+        reponseTerminal(
+          `<p>   ${dossiers
+            .find((dossier) => dossier.name === arguments[1])
+            .ls.join("   ")}</p>`
+        );
+      } else
+        reponseTerminal(
+          `<p>   <span class="error">error: folder "${arguments[1]}" does not exist</span></p>`
+        );
+    } else if (arguments[0] === "cd") {
+      if (arguments.length === 1) dossierActuel = "~";
+      else if (
+        dossierActuel === "~" &&
+        dossiers
+          .find((dossier) => dossier.name === dossierActuel)
+          .childs.includes(arguments[1])
+      )
+        dossierActuel = arguments[1];
+      else if (arguments[1] === ".." && dossierActuel !== "~") {
+        dossierActuel = "~";
+      } else
+        reponseTerminal(
+          `<p>   <span class="error">error: folder "${arguments[1]}" does not exist</span></p>`
+        );
+    } else if (arguments[0] === "clear") {
       // Sélectionner tous les éléments avec la classe "ancienneLigne"
       var elements = document.getElementsByClassName("ancienneLigne");
 
@@ -143,6 +102,14 @@ document.addEventListener("keydown", async function (event) {
       elementsArray.forEach(function (element) {
         element.remove();
       });
+    } else if (arguments[0] === "pwd") {
+      reponseTerminal(
+        `<p>
+         <span class="comment">${
+        dossiers.find((dossier) => dossier.name === dossierActuel).pwd
+      }</span>
+     </p>`
+      );
     } else if (commande.trim().length >= 1) {
       reponseTerminal(
         `<p>   <span class="error">error: "${commande}" command not found</span></p>`
@@ -159,8 +126,10 @@ document.addEventListener("keydown", async function (event) {
     ligneActuelle.classList.add("lignes");
     ligneActuelle.classList.add("ligneActuelle");
 
-    ligneActuelle.innerHTML = `<p><span class="information">visitor@semanavasco</span><span class="normalized">:</span><span class="comment">${dossierActuel}</span><span class="information">$</span> </p>
-         <input type="text" name="zonetexte" id="zonetexte" autofocus>`;
+    ligneActuelle.innerHTML = `<p><span class="information">visitor@semanavasco</span><span class="normalized">:</span><span class="comment">${
+      dossiers.find((dossier) => dossier.name === dossierActuel).terminal
+    }</span><span class="information">$</span> </p>
+         <input type="text" name="zonetexte" id="zonetexte" maxlength="100" autofocus>`;
 
     var zoneTexte = document.getElementById("zonetexte");
 
