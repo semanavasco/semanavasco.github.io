2f6c6390e52af6c77fcd2bfa2271097fae717a41
svasco <semanavasco@gmail.com>
2023-12-16 13:18:46 +0100

    Added informations about the Solo Leveling project

--- scripts/fonctions.js
+++ scripts/fonctions.js
@@ -117,9 +117,159 @@ function ouvrirProjet(projet) {
 
       // Ajout du contenu dans la fenêtre du projet
       var contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("titreProjet");
       contenu.innerHTML =
-        '<span class="normalized">Solo Leveling - RPG Discord</span>';
+        '<span class="information">Solo Leveling - Discord RPG</span>';
+      contenu.style.fontWeight = "bold";
+      contenu.style.fontSize = "1.5em";
+      contenu.style.marginTop = "2%";
+      contenu.style.marginLeft = "2%";
 
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">Le projet Solo Leveling consistait à créer un Bot Discord de type RPG basé sur le manhwa Solo Leveling. Celui-ci offrait la possiblité de créer un personnage, l'équiper avec plus de 500 pièces d'équipement pour affronter des ennemis avec nos amis.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("sousTitre1Projet");
+      contenu.innerHTML = `<span class="question">Développement</span>`;
+      contenu.style.fontWeight = "bold";
+      contenu.style.fontSize = "1.5em";
+      contenu.style.marginLeft = "2%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- C'est un projet développé en JavaScript à l'aide de la librairie Discord.JS.</span><br/><span class="normalized">- Je l'ai développé entièrement seul et l'ai recommencé plusieurs fois au fûr et à mesure que j'apprenais de nouvelles choses.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("sousTitre2Projet");
+      contenu.innerHTML = `<span class="question">Images</span>`;
+      contenu.style.fontWeight = "bold";
+      contenu.style.fontSize = "1.5em";
+      contenu.style.marginLeft = "2%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Retour lors de la création du personnage récapitulant toutes les informations de celui-ci (classe, métier, etc).</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("img"));
+      contenu.classList.add("imageProjet");
+      contenu.src = "assets/images/sololeveling/personnage.png";
+      contenu.alt = "Création du personnage.";
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Affichage du profil du joueur avec la commande /profil. Les barres de vie, mana et exp sont réactives.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("img"));
+      contenu.classList.add("imageProjet");
+      contenu.src = "assets/images/sololeveling/profil.png";
+      contenu.alt = "Affichage du profil du joueur.";
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Affichage de l'inventaire du joueur avec la commande /inventaire, celui-ci se mets à jour automatiquement lorsque le joueur déséquipe/équipe un item avec /déséquiper ou /équiper.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("img"));
+      contenu.classList.add("imageProjet");
+      contenu.src = "assets/images/sololeveling/inventaire.png";
+      contenu.alt = "Affichage de l'inventaire du joueur.";
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Affichage de la monnaie (ayant une vraie valeur économique, elle permet d'acheter des items, appartements, etc) du joueur ainsi que la quantité de cristaux (matériau d'amélioration) possédés.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("img"));
+      contenu.classList.add("imageProjet");
+      contenu.src = "assets/images/sololeveling/monnaies.png";
+      contenu.alt = "Affichage de la monnaie du joueur.";
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Affichage du groupe du joueur avec la commande /groupe. Ce groupe permets aux joueurs d'automatiquement partager les récompenses entre eux lors des affrontements.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("img"));
+      contenu.classList.add("imageProjet");
+      contenu.src = "assets/images/sololeveling/groupes.png";
+      contenu.alt = "Affichage du groupe du joueur.";
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Système d'intéractions avec  des PNJs (personnages non joueurs), ayant chacun des dialogues et possiblités différentes. Plus de 50 PNJs ont été créés, cela englobe leur histoire, leur caractère, leurs dialogues, etc.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("video"));
+      contenu.classList.add("imageProjet");
+      contenu.controls = true;
+      contenu.innerHTML = `<source src="assets/images/sololeveling/pnjs.mp4" type="video/mp4">...`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Système d'échange entre joueurs et de gestion de l'inventaire en permettant aux joueurs de lâcher des objets dans la carte.</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("video"));
+      contenu.classList.add("imageProjet");
+      contenu.controls = true;
+      contenu.innerHTML = `<source src="assets/images/sololeveling/echange.mp4" type="video/mp4">...`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
+
+      contenu = contenuFenetre.appendChild(document.createElement("p"));
+      contenu.classList.add("courteDescriptionProjet");
+      contenu.innerHTML = `<span class="normalized">- Un système de récompenses basé sur l'activité du joueur et proposant une version payante (mais pouvant être obtenue gratuitement par la complétion de certaines quêtes).</span>`;
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+
+      contenu = contenuFenetre.appendChild(document.createElement("img"));
+      contenu.classList.add("imageProjet");
+      contenu.src = "assets/images/sololeveling/recompenses.png";
+      contenu.alt = "Système de récompenses d'activité.";
+      contenu.style.marginLeft = "10%";
+      contenu.style.marginRight = "10%";
+      contenu.style.width = "80%";
+      contenu.style.height = "auto";
       break;
 
     default:
