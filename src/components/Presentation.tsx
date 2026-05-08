b283029721400ab2c2448f604978ed4e22e8962e
semanavasco <semanavasco@gmail.com>
2026-05-08 17:24:47 +0200

    feat: add resume button to presentation section

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -19,17 +19,28 @@ export const Presentation = ({ presentation }: Props) => {
       </div>
 
       <div className="socials">
+        <a href="/assets/docs/resume.pdf" target="_blank" rel="noopener noreferrer">
+          <span className="material-symbols-rounded">description</span>
+          Resume
+        </a>
         {presentation.socials.github && (
           <a href={presentation.socials.github} target="_blank" rel="noopener noreferrer">
+            <i className="devicon-github-original"></i>
             GitHub
           </a>
         )}
         {presentation.socials.linkedin && (
           <a href={presentation.socials.linkedin} target="_blank" rel="noopener noreferrer">
+            <i className="devicon-linkedin-plain"></i>
             LinkedIn
           </a>
         )}
-        {presentation.socials.email && <a href={presentation.socials.email}>Email</a>}
+        {presentation.socials.email && (
+          <a href={presentation.socials.email}>
+            <span className="material-symbols-rounded">mail</span>
+            Email
+          </a>
+        )}
       </div>
     </div>
   );
